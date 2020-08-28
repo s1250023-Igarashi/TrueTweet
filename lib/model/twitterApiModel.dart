4160cc74a0628566674b43331a955c328395dfe1
black315 <ryo.0301.1999@gmail.com>
2020-08-29 07:19:13 +0900

    fix item of tweet

--- lib/model/twitterApiModel.dart
+++ lib/model/twitterApiModel.dart
@@ -6,15 +6,14 @@ import 'tweetModel.dart';
 
 class TwitterApi {
   static final String apiKey = 'API key';
-  static final String apiSecret = 'API secret key';
-  static final String accessToken = 'access token';
+  static final String apiSecret = 'API secret';
+  static final String accessToken = 'Access token';
   static final String accessSecret = 'access secret';
 
   static Future<List<Tweet>> getTimeLine() async {
     Twitter twitter = new Twitter(apiKey, apiSecret, accessToken, accessSecret);
 
-    String response = await twitter.request(
-        'get', 'statuses/home_timeline.json', {'count': '10', 'include_entities': 'true',});
+    String response = await twitter.request('get', 'statuses/home_timeline.json', {'count': '10', 'include_entities': 'true',});
     
     final tweetsJson = jsonDecode(response);
 
@@ -24,44 +23,27 @@ class TwitterApi {
       User user = new User(tweetsJson[i]['user']['name'],
           tweetsJson[i]['user']['screen_name'],
           tweetsJson[i]['user']['profile_image_url_https'],
-          "banner",
-          "bio",
-          0,
-          0,
-          true);
+          tweetsJson[i]['user']["profile_banner_url"],
+          tweetsJson[i]['user']['description'],
+          tweetsJson[i]['user']['friends_count'],
+          tweetsJson[i]['user']['followers_count'],
+          tweetsJson[i]['user']['verified']
+      );
 
       Tweet tweet = new Tweet(user,
           tweetsJson[i]['text'],
-          '',
+          '', // TODO: Image URL
           tweetsJson[i]['favorite_count'],
           tweetsJson[i]['favorited'],
           tweetsJson[i]['retweet_count'],
           tweetsJson[i]['retweeted'],
           tweetsJson[i]['reply_count'],
-          "0");
+          '' // TODO: Timestamp
+      );
 
       tweets.add(tweet);
     }
 
     return tweets;
-  }
-
-  static String timeAgo(int timestamp) {
-    final currentTime = DateTime.now().millisecondsSinceEpoch;
-    final timeDiff = currentTime - timestamp;
-    if (timeDiff >= (1000 * 60 * 60 * 24)) {
-      // Days
-      return '${timeDiff ~/ (1000 * 60 * 60 * 24)}d';
-    } else if (timeDiff >= (1000 * 60 * 60)) {
-      // Hours
-      return '${timeDiff ~/ (1000 * 60 * 60)}h';
-    } else if (timeDiff >= (1000 * 60)) {
-      // Minutes
-      return '${timeDiff ~/ (1000 * 60)}m';
-    } else if (timeDiff >= 1000) {
-      // Seconds
-      return '${timeDiff ~/ 1000}s';
-    }
-    return '0s';
   }
 }
